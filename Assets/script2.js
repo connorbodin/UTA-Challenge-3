e5139ffe728e9580aafac605ab411a336cd1b6e1
connorbodin <connor.e.bodin@gmail.com>
2022-12-28 11:09:59 -0600

    added additional java script files and lines of code

--- Assets/script2.js
+++ Assets/script2.js
@@ -1,15 +1,15 @@
-  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-  var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-  var max_count = 5;
-  var confirmChar = '';
-  var confirmUpper = '';
-  var confirmLower = '';
-  var confirmSpecial = '';
-  var confirmNum = '';
-  // combines all arrays 
-  var allArray = upperCase.concat(lowerCase, num, specialChar);
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var max_count = 5;
+var confirmChar = '';
+var confirmUpper = '';
+var confirmLower = '';
+var confirmSpecial = '';
+var confirmNum = '';
+// combines all arrays 
+var allArray = upperCase.concat(lowerCase, num, specialChar);
 
 var generateBtn = document.querySelector("#generate");
 
@@ -34,61 +34,84 @@ function generatePassword() {
   console.log(confirmChar); {
 
   }
-  alert("You chose " + confirmChar + " number of charactlers! Cool cool, lets create a password with " +confirmChar + ' characters!');
+  alert("You chose " + confirmChar + " number of charactlers! Cool cool, lets create a password with " + confirmChar + ' characters!');
 
-  function askUpper () {
+  function askUpper() {
     var confirmUpper = prompt('Would you like upper case characters?', 'y/n');
     confirmUpper = confirmUpper.toLowerCase();
 
-  if (confirmUpper == 'y'){
-    alert('You chose ' +confirmUpper + '. We will include upper case characters!');
-    console.log(confirmUpper);
-  } else if (confirmUpper == 'n') {
-    alert('You chose ' +confirmUpper + '. We wont inclue upper case characters!');   
-    console.log(confirmUpper);
-  } else {
-  alert('You didnt select a valid option of y or num, please select y or n!');
-  askUpper(); 
+    if (confirmUpper == 'y') {
+      alert('You chose ' + confirmUpper + '. We will include upper case characters!');
+      console.log(confirmUpper);
+    } else if (confirmUpper == 'n') {
+      alert('You chose ' + confirmUpper + '. We wont inclue upper case characters!');
+      console.log(confirmUpper);
+    } else {
+      alert('You didnt select a valid option of y or num, please select y or n!');
+      askUpper();
+    }
+
+  }
+  askUpper();
+
+  function askLower() {
+    var confirmLower = prompt('Would you like upper case characters?', 'y/n');
+    confirmLower = confirmLower.toLowerCase();
+
+    if (confirmLower == 'y') {
+      alert('You chose ' + confirmLower + '. We will include lower case characters!');
+      console.log(confirmLower);
+    } else if (confirmLower == 'n') {
+      alert('You chose ' + confirmLower + '. We wont inclue lower case characters!');
+      console.log(confirmLower);
+    } else {
+      alert('You didnt select a valid option of y or num, please select y or n!');
+      askLower();
+    }
   }
 
-}
-askUpper();
+  askLower();
 
-function askLower() {
-  var confirmLower = prompt('Would you like upper case characters?', 'y/n');
-  confirmLower = confirmLower.toLowerCase();
+  function addNumbers() {
+    var confirmNum = prompt('Would you like confirmNumbers? ', 'y/n');
+    confirmNum = confirmNum.toLowerCase();
 
-if (confirmLower == 'y'){
-  alert('You chose ' +confirmLower + '. We will include lower case characters!');
-  console.log(confirmLower);
-} else if (confirmLower == 'n') {
-  alert('You chose ' +confirmLower + '. We wont inclue lower case characters!');   
-  console.log(confirmLower);
-} else {
-alert('You didnt select a valid option of y or num, please select y or n!');
-askLower(); 
-}
-}
+    if (confirmNum == 'y') {
+      alert('You chose ' + confirmNum + '. We will include numbers!');
+      console.log(confirmNum);
+    } else if (confirmNum == 'n') {
+      alert('You chose ' + confirmNum + '. We wont include numbers!');
+      console.log(confirmNum);
+    } else {
+      alert('You didnt select a valid option of y or n, please select y or n!');
+      addNumbers();
+    }
+  }
 
-askLower();
+  addNumbers();
 
-function addNumbers() {
-  var num = prompt('Would you like numbers? ', 'y/n');
-  num = num.toLowerCase();
+  function addSpecial() {
+    var specialChar = prompt('Would you like special characters?', 'y/n');
+    specialChar = specialChar.toLowerCase();
 
-if (num == 'y'){
-  alert('You chose ' +num + '. We will include numbers!');
-  console.log(num);
-} else if (num == 'n') {
-  alert('You chose ' +num + '. We wont include numbers!');   
-  console.log(num);
-} else {
-alert('You didnt select a valid option of y or n, please select y or n!');
-addNumbers(); 
-}
-}
+    if (specialChar == 'y') {
+      alert('You chose ' + specialChar + '. We will include special characters!');
+      console.log(specialChar);
+    } else if (specialChar == 'n') {
+      alert('You chose ' + specialChar + '. We wont special characters numbers!');
+      console.log(specialChar);
+    } else {
+      alert('You didnt select a valid option of y or n, please select y or n!');
+      addSpecial();
+    }
+  }
 
-addNumbers();
+  addSpecial();
+
+  
+  alert('You chose ' +confirmChar + ' characters, ' + confirmUpper + ' to upper case characters, ' + confirmLower + ' to lower case characters, ' + confirmNum + ' to including numbers, and ' + confirmSpecial + ' to special characters! ');
+  
+
 
   // var confirmLower = prompt('Would you like lower case characters?', 'y/n');
   // var confirmSpecial = prompt('Would you like special characters?', 'y/n')
@@ -117,5 +140,8 @@ var allArray = upperCase.concat(lowerCase, num, specialChar);
 
 writePassword();
 
-console.log (allArray);
+console.log(allArray);
 console.log(confirmUpper);
+console.log(confirmSpecial);
+console.log(confirmChar);
+

03c5ae31d5a21801bc86fd21bec760cd4f93559f
connorbodin <connor.e.bodin@gmail.com>
2023-01-03 23:32:10 -0600

    added multiple lines of codes and different variations I used to complete this project

--- Assets/script.js
+++ Assets/script.js
@@ -1,83 +1,128 @@
 // // Assignment Code
 // var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var generateBtn = document.querySelector("#generate");
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var max_count = 5;
+var confirmChar = '';
+var confirmUpper = '';
+var confirmLower = '';
+var confirmNum = '';
+var confirmSpecial = '';
+var allArr = {
+  upper: upperCase,
+  lower: lowerCase,
+  special: specialChar, 
+  numbers: num
+};
 
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('password');
-
-  passwordText.value = password;
-}
+//empty array to concat the 'y' answers together
+var passChars = [];
 
 generateBtn.addEventListener("click", writePassword);
 
-  // }
-  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-  var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-  var max_count = 5;
-  var confirmChar = '';
-  var confirmUpper = '';
-  var confirmLower = '';
-  var confirmSpecial = '';
-  var confirmNum = '';
-  //  Add event listener to generate button
-  // generateBtn.addEventListener("click", writePassword);
 
-  function generatePassword() {
-    //    must return a string value that is the password
-    //    prompt user to choose num btwn 8 -128
+function generatePassword() {
+  //    prompt user to choose num btwn 8 - 128
+  var confirmChar = prompt('Please select a numerical value between 8 and 128');
+
+  while (confirmChar <= 7 || confirmChar >= 129 || isNaN(confirmChar)) {
+    alert('Password length must be between 8 and 128, please try again!');
     var confirmChar = prompt('Please select a numerical value between 8 and 128');
-
-    while (confirmChar <= 7 || confirmChar >= 129 || null === true || '' === true) {
-      alert('Password length must be between 8 and 128, please try again!');
-      var confirmChar = prompt('Please select a numerical value between 8 and 128');
-    }
-    console.log(confirmChar); {
-
-    }
-    alert("You chose " + confirmChar + " number of charactlers! Cool cool, lets create a password with " +confirmChar + ' characters!')
-
-    var confirmUpper = prompt('Would you like upper case letters? y/n?', 'y/n');
-    var confirmLower = prompt('Would you like lower case characters?', 'y/n');
-    var confirmSpecial = prompt('Would you like special characters?', 'y/n')
-    var confirmNum = prompt('Would you like numbers?', 'y/n');
-
-
-  
-
-    //   var to save length
-    //  validate user number
-    // confirm user upper, lower, num, special characters - 4 confirm prompts
-    //   4 vars to safe T / F to included characters 
-    //  Vars that include all user options of each category.array of string for each category
-    //  EX.var upper = ['A' 'B']
-    //   if user says yes to include that category choose random elements from the array 
-    //   How to decide how many elements to choose from each array 
-    //   validate that atleast one cat was chosen
-    //   if x is true do this if x and x is true do this is x is true and y is false do this
-    //   join chosen elements into a string
-    //   console log every variable based on user input
-
-    //   return passwordString;
-
-    console.log(upperCase);
-    console.log(specialChar);
-    console.log(lowerCase);
-    console.log(num);
-    console.log(confirmChar);
-    console.log(confirmUpper);
-    console.log(confirmLower);
-    console.log(confirmNum);
-
+  }
+  console.log(confirmChar); {
 
   }
+  alert("You chose " + confirmChar + " number of charactlers! Cool cool, lets create a password with " + confirmChar + ' characters!')
+  // Confirming for upper case characters
+  var confirmUpper = prompt('Would you like upper case letters? y/n?', 'y/n');
 
-writePassword();
+  while (confirmUpper != 'y' && confirmChar != 'n') {
+    alert('Must be an input of y or n!');
+    var confirmChar = prompt('Would you like upper case letters', 'y/n');
+  }
+  alert("You chose " + confirmUpper + ' to the option to include uppercase characters!')
 
+  console.log(confirmUpper);
+
+  // Confirming for lower case characters
+
+  var confirmLower = prompt('Would you like lower case characters?', 'y/n');
+
+  while (confirmLower != 'y' && confirmLower != 'n') {
+    alert('Must be an input of y or n!');
+    var confirmChar = prompt('Would you like lower case letters', 'y/n');
+  }
+  alert("You chose " + confirmLower + ' to the option to include lower case characters!');
+
+  console.log(confirmLower);
+
+  // Confirming for number characters
+
+  var confirmNum = prompt('Would you like numbers?', 'y/n');
+
+  while (confirmNum != 'y' && confirmNum != 'n') {
+    alert('Must be an input of y or n!');
+    var confirmNum = prompt('Would you like numbers?', 'y/n');
+  }
+  alert("You chose " + confirmNum + ' to the option to include numbers!');
+  console.log(confirmNum);
+
+  // Confirming for upper Special characters
+
+
+  var confirmSpecial = prompt('Would you like special characters?', 'y/n');
+  while (confirmSpecial != 'y' && confirmSpecial != 'n') {
+    alert('Must be an input of y or n!');
+    var confirmChar = prompt('Would you like lower case letters', 'y/n');
+  }
+  alert("You chose " + confirmSpecial + ' to the option to include special characters!');
+  console.log(confirmSpecial);
+
+  // if statement if choosing n for all options 
+
+  if (confirmSpecial === 'n' && confirmUpper === 'n' && confirmLower === 'n' && confirmNum === 'n') {
+    alert('You must choose y to atleast one option!')
+  } else {
+    alert('You chose ' + confirmChar + ' characters, ' + confirmUpper + ' to upper case characters, ' + confirmLower + ' to lower case characters, ' + confirmNum + ' to numbers and ' + confirmSpecial + ' to special characters! ')
+  }
+
+
+// confirms if each confirmation is matching to 'y' and concats the arrays together
+if (confirmUpper === 'y') {
+  passChars = passChars.concat(upperCase)
+} 
+
+if (confirmLower === 'y') {
+  passChars = passChars.concat(lowerCase)
+}
+
+if (confirmSpecial === 'y') {
+  passChars = passChars.concat(specialChar)
+}
+
+if (confirmNum === 'y') {
+  passChars = passChars.concat(num)
+}
+
+console.log(passChars); 
+
+var randPw = '';
+
+for (var i = 0; i < confirmChar; i++) {
+  randPw = randPw + passChars[Math.floor((Math.random() * passChars.length))];
+  console.log(randPw);
+} 
+return randPw
+
+}
+// writes password generated to the text field
+function writePassword() {
+  var password = generatePassword();
+  var randPW = document.querySelector('#password');
+
+  randPW.value = password;
+}
 
--- Assets/script2.js
+++ Assets/script2.js
@@ -1,147 +1,47 @@
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-var max_count = 5;
-var confirmChar = '';
-var confirmUpper = '';
-var confirmLower = '';
-var confirmSpecial = '';
-var confirmNum = '';
-// combines all arrays 
-var allArray = upperCase.concat(lowerCase, num, specialChar);
 
-var generateBtn = document.querySelector("#generate");
+// This JS file is randome things I tried. 
+var randPW = [];
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('password');
 
-  // passwordText.value = password;
+
+if (confirmSpecial === 'y' && confirmUpper === 'n' && confirmLower === 'n' && confirmNum === 'n') {
+  randPW = specialChar;
+} else if (confirmSpecial === 'y' && confirmUpper === 'y' && confirmLower === 'n' && confirmNum === 'n') {
+  randPW = specialChar.concat(upperCase)
+} else if (confirmSpecial === 'y' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'n') {
+  randPW = specialChar.concat(upperCase, lowerCase) 
+} else if (confirmSpecial === 'y' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'y')
+  randPW = specialChar.concat(upperCase, lowerCase, num)
+
+else if (confirmSpecial === 'n' && confirmUpper === 'y' && confirmLower === 'n' && confirmNum === 'n') {
+  randPW = confirmUpper;
+} else if (confirmSpecial === 'n' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'n') {
+  randPW = upperCase.concat(lowerCase)
+} else if (confirmSpecial === 'n' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'y')
+  randPW = upperCase.concat(lowerCase, num)
+
+
+  else if (confirmSpecial === 'y' && confirmUpper === 'n' && confirmLower === 'y' && confirmNum === 'n') {
+    randPW = specialChar.concat(lowerCase) 
+  } else if (confirmSpecial === 'y' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'n') {
+    randPW = specialChar.concat(upperCase, lowerCase)
+  } else if (confirmSpecial === 'y' && confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'y')
+    randPW = specialChar.concat(upperCase, lowerCase, num)
+
+console.log(randPW);
+
+var randPW = '';
+
+if (confirmUpper === 'y') {
+randPW = upperCase
+} else if (confirmUpper === 'y' && confirmLower === 'y') {
+  randPW = upperCase.concat(lowerCase)
+} else if (confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'y') {
+randPW = upperCase.concat(lowerCase, num) 
+} else if (confirmUpper === 'y' && confirmLower === 'y' && confirmNum === 'y' && confirmSpecial === 'y') {
+randPW = upperCase.concat(lowerCase, num, specialChar) 
+} else {
+  
 }
 
-generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  //    must return a string value that is the password
-  //    prompt user to choose num btwn 8 -128
-  var confirmChar = prompt('Please select a numerical value between 8 and 128');
-
-  while (confirmChar <= 7 || confirmChar >= 129 || null === true || '' === true) {
-    alert('Password length must be between 8 and 128, please try again!');
-    var confirmChar = prompt('Please select a numerical value between 8 and 128');
-  }
-  console.log(confirmChar); {
-
-  }
-  alert("You chose " + confirmChar + " number of charactlers! Cool cool, lets create a password with " + confirmChar + ' characters!');
-
-  function askUpper() {
-    var confirmUpper = prompt('Would you like upper case characters?', 'y/n');
-    confirmUpper = confirmUpper.toLowerCase();
-
-    if (confirmUpper == 'y') {
-      alert('You chose ' + confirmUpper + '. We will include upper case characters!');
-      console.log(confirmUpper);
-    } else if (confirmUpper == 'n') {
-      alert('You chose ' + confirmUpper + '. We wont inclue upper case characters!');
-      console.log(confirmUpper);
-    } else {
-      alert('You didnt select a valid option of y or num, please select y or n!');
-      askUpper();
-    }
-
-  }
-  askUpper();
-
-  function askLower() {
-    var confirmLower = prompt('Would you like upper case characters?', 'y/n');
-    confirmLower = confirmLower.toLowerCase();
-
-    if (confirmLower == 'y') {
-      alert('You chose ' + confirmLower + '. We will include lower case characters!');
-      console.log(confirmLower);
-    } else if (confirmLower == 'n') {
-      alert('You chose ' + confirmLower + '. We wont inclue lower case characters!');
-      console.log(confirmLower);
-    } else {
-      alert('You didnt select a valid option of y or num, please select y or n!');
-      askLower();
-    }
-  }
-
-  askLower();
-
-  function addNumbers() {
-    var confirmNum = prompt('Would you like confirmNumbers? ', 'y/n');
-    confirmNum = confirmNum.toLowerCase();
-
-    if (confirmNum == 'y') {
-      alert('You chose ' + confirmNum + '. We will include numbers!');
-      console.log(confirmNum);
-    } else if (confirmNum == 'n') {
-      alert('You chose ' + confirmNum + '. We wont include numbers!');
-      console.log(confirmNum);
-    } else {
-      alert('You didnt select a valid option of y or n, please select y or n!');
-      addNumbers();
-    }
-  }
-
-  addNumbers();
-
-  function addSpecial() {
-    var specialChar = prompt('Would you like special characters?', 'y/n');
-    specialChar = specialChar.toLowerCase();
-
-    if (specialChar == 'y') {
-      alert('You chose ' + specialChar + '. We will include special characters!');
-      console.log(specialChar);
-    } else if (specialChar == 'n') {
-      alert('You chose ' + specialChar + '. We wont special characters numbers!');
-      console.log(specialChar);
-    } else {
-      alert('You didnt select a valid option of y or n, please select y or n!');
-      addSpecial();
-    }
-  }
-
-  addSpecial();
-
-  
-  alert('You chose ' +confirmChar + ' characters, ' + confirmUpper + ' to upper case characters, ' + confirmLower + ' to lower case characters, ' + confirmNum + ' to including numbers, and ' + confirmSpecial + ' to special characters! ');
-  
-
-
-  // var confirmLower = prompt('Would you like lower case characters?', 'y/n');
-  // var confirmSpecial = prompt('Would you like special characters?', 'y/n')
-  // var confirmNum = prompt('Would you like numbers?', 'y/n');
-
-
-
-
-  //   var to save length
-  //  validate user number
-  // confirm user upper, lower, num, special characters - 4 confirm prompts
-  //   4 vars to safe T / F to included characters 
-  //  Vars that include all user options of each category.array of string for each category
-  //  EX.var upper = ['A' 'B']
-  //   if user says yes to include that category choose random elements from the array 
-  //   How to decide how many elements to choose from each array 
-  //   validate that atleast one cat was chosen
-  //   if x is true do this if x and x is true do this is x is true and y is false do this
-  //   join chosen elements into a string
-  //   console log every variable based on user input
-
-  //   return passwordString;
-}
-
-var allArray = upperCase.concat(lowerCase, num, specialChar);
-
-writePassword();
-
-console.log(allArray);
-console.log(confirmUpper);
-console.log(confirmSpecial);
-console.log(confirmChar);
-
+confirms if each confirmation is matching to 'y' and concats the arrays together
